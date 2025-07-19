a9113f8d2ce556037c36d305594ccd30c3038a8c
A_Sharaawy <abdallahsharawy200@gmail.com>
2025-07-19 15:49:21 +0300

    last update

--- Day13/Linqu.cs
+++ Day13/Linqu.cs
@@ -63,6 +63,62 @@ namespace DaysCsharp.Day13
 			return result;
 
 		}
+		public static List<T> TakeWhile<T>(this List<T> source, Func<T, bool> predicate)
+		{
+			List<T> result = new List<T>();
+			for (int i = 0; i < source.Count(); i++)
+			{
+				if (predicate(source[i]))
+				{
+					result.Add(source[i]);
+				}else return result;
+			}
+			return result;
+		}
+		public static List<T> TakeWhile<T>(this List<T> source, Func<T,int,bool> predicate)
+		{
+			List<T> result = new List<T>();
+			for (int i = 0; i < source.Count(); i++)
+			{
+				if (predicate(source[i],i))
+				result.Add(source[i]);
+				else return result;
+
+			}
+			return result;
+
+		}
+		public static List<T> SkipWhile<T>(this List<T> source, Func<T, bool> predicate)
+		{
+			List<T> result = new List<T>();
+			int count = 0;
+			for (int i = 0; i < source.Count(); i++)
+			{
+				if (predicate(source[i]))
+				{
+					count++;
+				}
+				else break;
+			}
+			for (int i = count; i < source.Count(); i++)
+			{
+				result.Add(source[i]);
+			}
+			return result;
+		}
+		public static List<T> SkipWhile<T>(this List<T> source, Func<T, int, bool> predicate)
+		{
+			List<T> result = new List<T>();
+			for (int i = 0; i < source.Count(); i++)
+			{
+				if (predicate(source[i], i))
+					result.Add(source[i]);
+				else return result;
+
+			}
+			return result;
+
+		}
 		public static List<T> Skip<T>(this List<T> source, int NumElements)
 		{
 			List<T> result = new List<T>();
